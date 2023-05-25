a93826142af2e4718135fbecd1708854101a34e4
OleksandrShpachuk <al.shpachuk@gmail.com>
2023-05-25 12:02:53 +0300

    Done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryElement = document.querySelector(".gallery");
+
+const markupForGallery = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+    </a>
+  </div>
+`
+  )
+  .join("");
+
+galleryElement.insertAdjacentHTML("beforeend", markupForGallery);
+
+galleryElement.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+
+  galleryElement.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+});
 
 console.log(galleryItems);
